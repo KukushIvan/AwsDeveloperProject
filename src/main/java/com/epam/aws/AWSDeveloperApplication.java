772a8d03ceafad1905304dcfcb45919f84bbdbc0
Ivan Kukushkin <Ivan_Kukushkin@epam.com>
2024-07-27 00:19:49 +0100

    Initial commit with get /region-info method

---
+++ src/main/java/com/epam/aws/AWSDeveloperApplication.java
@@ -0,0 +1,11 @@
+package com.epam.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AWSDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AWSDeveloperApplication.class, args);
+    }
+}
