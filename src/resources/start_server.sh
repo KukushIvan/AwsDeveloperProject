75fd49f048ecc430ccd182bf90e317512eea7cdb
Ivan Kukushkin <Ivan_Kukushkin@epam.com>
2024-09-21 20:23:23 +0100

    Changes: Switched back to absolute patch, logging the env. variables

--- src/resources/start_server.sh
+++ src/resources/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/
+env > /home/ec2-user/env_vars.log
+source /etc/environment
+env > /home/ec2-user/env_vars_updated.log
 pkill -f 'java -jar /home/ec2-user/AwsDeveloperProject.jar'
 
 nohup java -jar AwsDeveloperProject.jar > app.log 2>&1 &
