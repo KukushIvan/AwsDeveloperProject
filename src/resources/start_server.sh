4d1ab60a65fed90e32bd0ae25692ccaa36c5e2ac
Ivan Kukushkin <Ivan_Kukushkin@epam.com>
2024-09-21 21:01:02 +0100

    Changes: Removed env credentials from /etc/environement since code-deploy uses non-login shells and could not get them.

--- src/resources/start_server.sh
+++ src/resources/start_server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/
+source /home/ec2-user/app_env.sh
 env > /home/ec2-user/env_vars.log
-source /etc/environment
-env > /home/ec2-user/env_vars_updated.log
 pkill -f 'java -jar /home/ec2-user/AwsDeveloperProject.jar'
 
 nohup java -jar AwsDeveloperProject.jar > app.log 2>&1 &
